9105368866d816e88e54ed79acaaa6ecaacf4087
Miguel Ibero <mibero@kailab.com>
2011-10-03 16:35:22 +0200

    removed english menu links

--- bin/database_reload.php
+++ bin/database_reload.php
@@ -3,4 +3,4 @@ php app/console --force doctrine:database:drop
 php app/console doctrine:database:create
 php app/console doctrine:schema:create
 php app/console fos:user:create admin admin@wowbile.eu admin01
-pgp app/consolse fos:user:promote admin
+php app/console fos:user:promote admin
